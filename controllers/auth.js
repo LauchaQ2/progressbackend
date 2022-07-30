454721de6a2eeb25a52dca3db1bba7d7968ec574
Lautaro Quevedo <89219479+LauchaQ2@users.noreply.github.com>
2022-07-30 14:45:26 -0300

    Google Sign added

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,15 +2,17 @@ const { response } = require("express");
 const User = require('../models/user');
 const bcrypt = require('bcrypt-nodejs');
 const { generateJWT } = require("../helpers/generate-jwt");
- 
+const { googleVerify } = require("../helpers/google-verify");
+const { json } = require("body-parser");
 
-const login = async(req, res = response) =>{
 
-    const {email, password} = req.body;
+const login = async (req, res = response) => {
+
+    const { email, password } = req.body;
 
     try {
-        const user = await User.findOne({email});
-        if ( !user ) {
+        const user = await User.findOne({ email });
+        if (!user) {
             return res.status(400).json({
                 msg: "Email isn't correct."
             })
@@ -29,7 +31,7 @@ const login = async(req, res = response) =>{
             })
         }
 
-         const token = await generateJWT( user.id );
+        const token = await generateJWT(user.id);
 
 
         res.json({
@@ -43,9 +45,56 @@ const login = async(req, res = response) =>{
         })
     }
 
-    
+
+}
+
+const googleSignIn = async (req, res = response) => {
+    const { id_token } = req.body;
+
+    try {
+        const { name, picture, email } = await googleVerify(id_token)
+
+        let user = await User.findOne({ email })
+
+        if (!user) {
+            const data = {
+                name,
+                email,
+                password: '.p',
+                role: 'USER_ROLE',
+                google: true
+            };
+
+            const salt = bcrypt.genSaltSync();
+
+            user = new User(data);
+            user.img = picture;
+            await user.save();
+        }
+
+        if (!user.state) {
+            res.status(401).json({
+                msg: 'Talk with Admin, user blocked'
+            });
+        }
+
+        const token = await generateJWT(user.id);
+
+
+        res.json({
+            user,
+            token
+        })
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'Token not verified'
+        })
+    }
+
 }
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
