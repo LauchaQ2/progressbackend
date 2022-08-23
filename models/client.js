015616bf2ec72d7b19b7062c4903350db800e081
Lautaro Quevedo <89219479+LauchaQ2@users.noreply.github.com>
2022-08-22 21:38:37 -0300

    .

--- models/client.js
+++ models/client.js
@@ -51,7 +51,7 @@ const ClientSchema = Schema({
         required: [true, 'Domicilio legal is required']
     },
     sedeDeExplotacion: {
-        type: Object,
+        type: Array,
         required: [true, 'Sede de Exp is required']
     },
     agencia: {
