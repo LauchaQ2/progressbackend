8d3691d680639ab0fa04e789d6676c4b726c11eb
LauchaQ2 <lautaroquevedocalzada@gmail.com>
2023-11-14 18:00:43 -0300

    .

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-var _app = _interopRequireDefault(require("./app.js"));
-_app["default"].listen(_app["default"].get('port'));
-console.log('server port:', _app["default"].get('port'));
