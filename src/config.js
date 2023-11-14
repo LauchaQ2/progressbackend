702f27aff4131cf15ff6c011343a0d831f5f4aa2
LauchaQ2 <lautaroquevedocalzada@gmail.com>
2023-11-14 18:45:36 -0300

    .

--- src/config.js
+++ src/config.js
@@ -6,6 +6,6 @@ export default {
     port: process.env.PORT || 3000,
     dbUser: process.env.DB_USER || '',
     dbPassword: process.env.DB_PASSWORD || '',
-    dbServer: process.env.DB_SERVER || '',
+    dbServer: '192.168.0.177' || '',
     dbDatabase: process.env.DB_DATABASE || ''
 }
