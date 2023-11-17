d1f47aac6397acb4d5a18dca492d89b72ef36dfb
LauchaQ2 <lautaroquevedocalzada@gmail.com>
2023-11-17 00:15:39 -0300

    .

--- src/controllers/upload.controller.js
+++ src/controllers/upload.controller.js
@@ -1,4 +1,6 @@
 const { authorize, uploadFile } = require('../googleDrive.js');
+const fs = require('fs').promises;
+const path = require('path');
 
 async function uploadImage(req, res) {
     const file = req.file;
@@ -10,6 +12,12 @@ async function uploadImage(req, res) {
         const authClient = await authorize();
         const uploadedFile = await uploadFile(authClient, file.originalname);
 
+        try {
+            await fs.unlink(path.join('uploads', file.originalname));
+        } catch (err) {
+            console.error('Error al eliminar el archivo local:', err);
+        }
+        
         res.status(200).json({ fileId: uploadedFile });
     } catch (err) {
         res.status(500).send('Error al subir el archivo a Google Drive: ' + err);
