6bde1aace3e8d9b0729c65563ba47ee8e81b1bde
LauchaQ2 <lautaroquevedocalzada@gmail.com>
2023-12-07 12:01:40 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 // app.use(cors({ origin: 'http://127.0.0.1:5173' }));
 app.use(cors({ 
-    origin: ['http://127.0.0.1:5174','http://127.0.0.1:5173'] }));
+    origin: ['http://127.0.0.1:5174','http://127.0.0.1:5173', 'https://progresscrud.netlify.app'] }));
 
 // app.use(productsRoutes);
 // app.use(productosColoresRoutes);
